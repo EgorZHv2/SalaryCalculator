da010a17fb6aa93c69139410dcbf60ac97bbf5b0
Егор <egor.zholobov@yandex.ru>
2022-04-21 13:36:10 +0300

    Начал писать MainViewModel

--- SalaryCalculator/ViewModels/BaseVm.cs
+++ SalaryCalculator/ViewModels/BaseVm.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace SalaryCalculator.ViewModels
+
 {
-    public class BaseVm
+    public class BaseVm:INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }
--- SalaryCalculator/ViewModels/MainViewModel.cs
+++ SalaryCalculator/ViewModels/MainViewModel.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace SalaryCalculator.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel:BaseVm
     {
+
     }
 }
