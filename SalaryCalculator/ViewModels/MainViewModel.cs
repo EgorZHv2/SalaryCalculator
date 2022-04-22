f591354339166827166d518ebffeb1764b151a24
Егор <egor.zholobov@yandex.ru>
2022-04-22 13:57:57 +0300

    Начал реализацию навигации

--- SalaryCalculator/ViewModels/MainViewModel.cs
+++ SalaryCalculator/ViewModels/MainViewModel.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Windows.Input;
+using System.Windows.Controls;
+using SalaryCalculator.Views.Pages;
 using SalaryCalculator.Services;
 using SalaryCalculator.Data;
 
@@ -12,10 +14,19 @@ namespace SalaryCalculator.ViewModels
 {
     public class MainViewModel:BaseVm
     {
+        private Page _page;
+
+        public Page Page
+        {
+            get { return _page; }
+            set { _page = value; }
+        }
+
         public MainViewModel()
         {
             DbInitializer.Initialize(ApplicationDbContext.GetContext());
-
+            PositionsPage pg = new PositionsPage();
+            Page = pg;
         }
     }
 }
--- SalaryCalculator/Views/MainWindow.xaml.cs
+++ SalaryCalculator/Views/MainWindow.xaml.cs
@@ -1,18 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
+﻿using System.Windows;
+using SalaryCalculator.Services;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
-using SalaryCalculator.Data;
 using SalaryCalculator.Views.Pages;
 
 namespace SalaryCalculator
@@ -25,9 +13,9 @@ namespace SalaryCalculator
         public MainWindow()
         {
             InitializeComponent();
+            Navigation.p = MainFrame.NavigationService;
             
-        }
 
-    
+        }
     }
 }
