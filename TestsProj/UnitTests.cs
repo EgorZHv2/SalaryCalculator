ffdd1ee0f381fb229193135a74ca094bf6793dc1
Егор <egor.zholobov@yandex.ru>
2022-04-28 16:55:48 +0300

    Добавил юнит тестов

---
+++ TestsProj/UnitTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace TestsProj
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
